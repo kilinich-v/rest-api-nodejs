77c00d06a391278b0e4f22c4298cd47058015409
Vladimir Kilinich <kylynych.v@gmail.com>
2021-06-21 21:55:07 +0300

    fix updateContact and updateStatusContact

--- validation/contactsValidations.js
+++ validation/contactsValidations.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 module.exports = {
   addContactValidation: (req, res, next) => {
     const schema = Joi.object({
-      name: Joi.string().alphanum().min(3).max(30).required(),
+      name: Joi.string().min(3).max(30).required(),
       email: Joi.string()
         .email({
           minDomainSegments: 1,
